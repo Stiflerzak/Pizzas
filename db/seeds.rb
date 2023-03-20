035c9a493b477b9c36c2dc1dece474ea9634275a
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-20 15:51:17 +0300

    Added records to restaurant_pizzas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 # Create pizza records
 
-Pizza.create(name: "Margherita", ingredients: "tomato sauce, mozzarella, basil")
-Pizza.create(name: "Pepperoni", ingredients: "tomato sauce, mozzarella, pepperoni")
-Pizza.create(name: "Hawaiian", ingredients: "tomato sauce, mozzarella, ham, pineapple")
-Pizza.create(name: "Meat Lover's", ingredients: "tomato sauce, mozzarella, sausage, bacon, pepperoni")
-Pizza.create(name: "Veggie", ingredients: "tomato sauce, mozzarella, bell peppers, onions, mushrooms")
+p1= Pizza.create(name: "Margherita", ingredients: "tomato sauce, mozzarella, basil")
+p2= Pizza.create(name: "Pepperoni", ingredients: "tomato sauce, mozzarella, pepperoni")
+p3= Pizza.create(name: "Hawaiian", ingredients: "tomato sauce, mozzarella, ham, pineapple")
+p4= Pizza.create(name: "Meat Lover's", ingredients: "tomato sauce, mozzarella, sausage, bacon, pepperoni")
+p5= Pizza.create(name: "Veggie", ingredients: "tomato sauce, mozzarella, bell peppers, onions, mushrooms")
 
 # Create restaurant records
-Restaurant.create(name: "Joe's Pizza", address: "123 Main St")
-Restaurant.create(name: "Papa John's", address: "456 Elm St")
-Restaurant.create(name: "Pizza Hut", address: "789 Oak St")
+r1= Restaurant.create(name: "Joe's Pizza", address: "123 Main St")
+r2= Restaurant.create(name: "Papa John's", address: "456 Elm St")
+r3= Restaurant.create(name: "Pizza Hut", address: "789 Oak St")
+
+
+# Create some restaurant pizza records
+
+r1.restaurant_pizzas.create(price: 30, pizza: p2)
+r2.restaurant_pizzas.create(price: 40, pizza: p4)
+r2.restaurant_pizzas.create(price: 50, pizza: p3)
+r3.restaurant_pizzas.create(price: 20, pizza: p1)
