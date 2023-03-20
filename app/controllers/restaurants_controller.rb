c9206372ddd4f79a969204b238cc05cfbff9255f
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-20 16:25:39 +0300

    added method to restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,7 @@
 class RestaurantsController < ApplicationController
+    def index
+        @restaurants = Restaurant.all
+    
+        render json: @restaurants.to_json(only: [:id, :name, :address])
+      end
 end
