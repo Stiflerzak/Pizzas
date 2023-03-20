fd628b4962e9512f98c82489ec6376111571d93a
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-20 15:31:17 +0300

    Added restaurant records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# Create some pizza records
+# Create pizza records
 
 Pizza.create(name: "Margherita", ingredients: "tomato sauce, mozzarella, basil")
 Pizza.create(name: "Pepperoni", ingredients: "tomato sauce, mozzarella, pepperoni")
 Pizza.create(name: "Hawaiian", ingredients: "tomato sauce, mozzarella, ham, pineapple")
 Pizza.create(name: "Meat Lover's", ingredients: "tomato sauce, mozzarella, sausage, bacon, pepperoni")
 Pizza.create(name: "Veggie", ingredients: "tomato sauce, mozzarella, bell peppers, onions, mushrooms")
+
+# Create restaurant records
+Restaurant.create(name: "Joe's Pizza", address: "123 Main St")
+Restaurant.create(name: "Papa John's", address: "456 Elm St")
+Restaurant.create(name: "Pizza Hut", address: "789 Oak St")
