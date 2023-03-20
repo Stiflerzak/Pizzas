a6d2a3de4c409a3f421f80ef247912220e86e253
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-20 17:07:46 +0300

    Added show method for a single restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,13 @@ class RestaurantsController < ApplicationController
     
         render json: @restaurants.to_json(only: [:id, :name, :address])
       end
+
+      def show
+        @restaurant = Restaurant.find_by(id: params[:id])
+        if @restaurant
+          render json: @restaurant.as_json(include: { pizzas: { only: [:id, :name, :ingredients] } })
+        else
+          render json: { error: "Restaurant not found" }, status: :not_found
+        end
+      end
 end
